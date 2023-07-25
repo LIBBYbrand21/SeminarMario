1b7e12cb087fd40408d5d7a28728c3497abd9bf3
LIBBYbrand21 <hand1b52mbjcomp.org.il>
2023-07-25 19:55:09 +0300

    start day 2

--- SeminarMario/pracrice.cpp
+++ SeminarMario/pracrice.cpp
@@ -1,49 +1,55 @@
 #include "Animation.h"
 #include <opencv2/opencv.hpp>
+
+#include "SlimeEntity.h"
+using namespace cv;
+
 #define HERO_DUCK 's'
 #define HERO_JUMP 'w'
 #define HERO_STAND_AFTER_DUCK 'w'
 #define HERO_RUN_RIGHT 'd'
 #define HERO_IDLE 'a'
 
-cv::Mat background = cv::imread(R"(../Animations/background.png)", cv::IMREAD_UNCHANGED);
+Mat background = imread(R"(../Animations/background.png)", cv::IMREAD_UNCHANGED);
+auto slime = CreateSlimeEnemy(R"(../Animations/SlimeOrange)");
 bool isToExit = false;
 
 
-int walkRight(cv::Point& topLeft);
-int idle(cv::Point& topLeft);
-int duck(cv::Point& topLeft);
-int jumpRight(cv::Point& topLeft);
+int walkRight(Point& topLeft);
+int idle(Point& topLeft);
+int duck(Point& topLeft);
+int jumpRight(Point& topLeft);
 
 
 int main()
 {
-	cv::resize(background, background, cv::Size(), 0.79, 0.75);
-	cv::Point topLeft(background.size().width / 2, background.size().height * 2.03 / 3);
+	resize(background, background, cv::Size(), 0.79, 0.75);
+	Point topLeft(background.size().width, background.size().height * 2.03 / 3);
 
-	idle(topLeft);
+	slime->reset(Point(background.size().width * 2 / 3, background.size().height * 4 / 5));
+
+	int key = idle(topLeft);
 
 	while (false == isToExit)
 	{
-		int key = cv::waitKey(100);
 		switch (key)
 		{
-		case 27: isToExit = true;
-		case HERO_DUCK: duck(topLeft);
-		case HERO_JUMP: key = jumpRight(topLeft);
-		case HERO_RUN_RIGHT:key = walkRight(topLeft);
-		case HERO_IDLE:key = idle(topLeft);
+		case 27: { isToExit = true; break; }
+		case HERO_DUCK: {key = duck(topLeft); break; }
+		case HERO_JUMP: {key = jumpRight(topLeft); break; }
+		case HERO_RUN_RIGHT: {key = walkRight(topLeft); break; }
+		case HERO_IDLE: {key = idle(topLeft); break; }
 		}
 	}
 
 
 	return 0;
 }
-void show(Animation& animation, cv::Point& topLeft, int x = 0, int y = 0) {
+void show(Animation& animation, Point& topLeft, int x = 0, int y = 0) {
 	for (int i = 0; i < animation.numFrames(); i++)
 	{
 		Frame const& frame = animation.getFrame(i);
-		cv::Mat canvas = background.clone();
+		Mat canvas = background.clone();
 		topLeft.x += x;
 		if (i < 5) {
 			topLeft.y -= y;
@@ -55,80 +61,80 @@ void show(Animation& animation, cv::Point& topLeft, int x = 0, int y = 0) {
 			topLeft.x = 0;
 		}
 		drawFrame(frame, canvas, topLeft);
-		cv::imshow("test", canvas);
-		int key = cv::waitKey(100);
+
+		// @2: slime is an "EntityState" and knows how to take care of itself:
+		slime->update();
+		slime->draw(canvas);
+
+
+		imshow("test", canvas);
+		int key = waitKey(100);
 	}
 }
 
-int walkRight(cv::Point& topLeft) {
+int walkRight(Point& topLeft) {
+	int x;
 	Animation animation(R"(../Animations/Hero/runRight)");
-	int key = cv::waitKey(100);
+	int key = waitKey(100);
 	while (key == -1) {
-		show(animation, topLeft, 10);
-		int key = cv::waitKey(100);
+		show(animation, topLeft, background.size().width/ animation.numFrames());
+		int key = waitKey(100);
 		if (key > 0) {
-			return key;
+			 x = key;
+			break;
 		}
 	}
+	return x;
 }
 
-int idle(cv::Point& topLeft) {
+int idle(Point& topLeft) {
+	int x=-1;
 	Animation animation(R"(../Animations/Hero/idle)");
-	int key = cv::waitKey(100);
+	int key = waitKey(100);
 	while (key == -1) {
 		show(animation, topLeft);
-		key = cv::waitKey(100);
+		key = waitKey(100);
 		if (key > 0) {
-			return key;
+			x = key;
+			break;
 		}
 	}
-
+	return x;
 }
 
-int duck(cv::Point& topLeft)
+int duck(Point& topLeft)
 {
+	int x;
 	Animation animation(R"(../Animations/Hero/duckDown)");
-	show(animation,topLeft);
+	show(animation, topLeft);
 	animation = Animation(R"(../Animations/Hero/duckStay)");
-	int key = cv::waitKey(100);
+	int key = waitKey(100);
 	while (key != HERO_STAND_AFTER_DUCK) {
 		Frame const& frame = animation.getFrame(0);
-		cv::Mat canvas = background.clone();
+		Mat canvas = background.clone();
 		drawFrame(frame, canvas, topLeft);
-		cv::imshow("test", canvas);
-		int key = cv::waitKey(100);
+		slime->update();
+		slime->draw(canvas);
+		imshow("test", canvas);
+		int key = waitKey(100);
 		if (key == HERO_STAND_AFTER_DUCK) {
+			x = key;
 			break;
 		}
 		if (key == 27) {
+			x = key;
 			isToExit = true;
-			return key;
+			break;
 		}
 	}
 	animation = Animation(R"(../Animations/Hero/standAfterDuck)");
 	show(animation, topLeft);
-	animation = Animation(R"(../Animations/Hero/idle)");
-	key = cv::waitKey(100);
-	while (key == -1) {
-		show(animation, topLeft);
-		key = cv::waitKey(100);
-		if (key > 0) {
-			return key;
-		}
-	}
+	return idle(topLeft);
 }
 
-int jumpRight(cv::Point& topLeft) {
+int jumpRight(Point& topLeft) {
 
 	Animation animation(R"(../Animations/Hero/jump)");
 	show(animation, topLeft, 20, 30);
-	animation = Animation(R"(../Animations/Hero/idle)");
-	int key = cv::waitKey(100);
-	while (key == -1) {
-		show(animation, topLeft);
-		key = cv::waitKey(100);
-		if (key > 0) {
-			return key;
-		}
-	}
+	return idle(topLeft);
 }
